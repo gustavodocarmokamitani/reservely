24a65ef2a3346700c4e9fea80475fcb48485440d
Gustavo Kamitani <gustavodocarmokamitani@gmail.com>
2025-02-24 14:10:49 -0300

    change axios url

--- src/axiosInstance.tsx
+++ src/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:7030/api/",
+  baseURL: "http://backendfoundation.onrender.com/api/",
   headers: {
     "Content-Type": "application/json",
   },
