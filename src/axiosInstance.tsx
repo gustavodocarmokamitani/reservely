d85b908bb269ce0bf91011c968c76e079595b0f0
Gustavo Kamitani <gustavodocarmokamitani@gmail.com>
2025-03-19 17:29:49 -0300

    fix: https *

--- src/axiosInstance.tsx
+++ src/axiosInstance.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
 
-  baseURL: "http://backendfoundation.onrender.com/",
+  baseURL: "https://backendfoundation.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
