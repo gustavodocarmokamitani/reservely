4337fb45ca123143656fdfe373b7c074be2542ae
Gustavo Kamitani <gustavodocarmokamitani@gmail.com>
2025-06-01 18:35:20 -0300

    fix: baseUrl

--- src/axiosInstance.tsx
+++ src/axiosInstance.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
 
   // baseURL: "http://localhost:7040/api",
-  baseURL: "https://api.reservely.com.br:7040/api",
+  baseURL: "http://api.reservely.com.br:7040/api",
   headers: {
     "Content-Type": "application/json",
   },
